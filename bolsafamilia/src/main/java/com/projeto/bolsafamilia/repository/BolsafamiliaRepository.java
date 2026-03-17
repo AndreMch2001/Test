966fe4db4ca30cac0e9e0cc20b2eaf7ac50bf35e
AndreMch2001 <0808andremachado@gmail.com>
2026-03-17 19:58:38 -0300

    Remoçao de métodos

--- bolsafamilia/src/main/java/com/projeto/bolsafamilia/repository/BolsafamiliaRepository.java
+++ bolsafamilia/src/main/java/com/projeto/bolsafamilia/repository/BolsafamiliaRepository.java
@@ -1,19 +1,12 @@
 package com.projeto.bolsafamilia.repository;
 import com.projeto.bolsafamilia.model.Bolsafamiliamodel;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
-public interface BolsafamiliaRepository extends JpaRepository<Bolsafamiliamodel, Long>, JpaSpecificationExecutor<Bolsafamiliamodel> {
-    
-    // O Spring traduz isso para o SQL que usa seu índice de nome automaticamente!
-    Page<Bolsafamiliamodel> findByNomeFavorecidoContainingIgnoreCase(String nome, Pageable pageable);
-    
-    // Busca por município
-    Page<Bolsafamiliamodel> findByNomeMunicipioIgnoreCase(String municipio, Pageable pageable);
+@Repository // Anotação que indica que a classe é um repositório JPA ou seja uma classe que contém os métodos para buscar os dados da API
+public interface BolsafamiliaRepository extends JpaRepository<Bolsafamiliamodel, Long>, JpaSpecificationExecutor<Bolsafamiliamodel> { // Interface que extende a classe JpaRepository e a classe JpaSpecificationExecutor
 }
